e5a9ee2cfdb77ebc6604d8304459732efdeea161
parthpatel1004 <parthpatel1004@gmail.com>
2020-11-02 09:25:05 +0530

    creating svg circle

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * Math.PI * 2;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(){
         console.log('time started');
     },
     onTick(){
-        console.log('timer ticked')
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset = currentOffset - 50;
     },
     onComplete(){
         console.log('timer completed')
     }
 });
+
+
+// NOTE : take svg referance from here : http://tutorials.jenkov.com/svg/circle-element.html
